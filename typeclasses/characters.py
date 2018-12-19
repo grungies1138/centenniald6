f8c921596dd2c02860dc571e253ae2ba7415e1fd
grungies1138 <grungies1138@gmail.com>
2018-12-19 16:26:21 -0600

    Setting up Server settings for initial start.

--- typeclasses/characters.py
+++ typeclasses/characters.py
@@ -40,19 +40,17 @@ class Character(DefaultCharacter):
 # Wit Skills: Medicine, Navigation, Mechanics, Languages, Science, Security, Survival, Perception, Law
 # Charm Skills: Command, Persuasion, Streetwise, Business, Politics, Intimidate, Gambling, Deception, Force
 
-# Perks ########################
-# Racial Perks
+## Perks ########################
+## Racial Perks
 
 # Custom(0)
 # Human(0)
 # Twi'lek(0)
 # Devaronian(0)
 # Arcona(0)
-# Wookie(2) - Claws, +1 to Might skills
-#
+# Wookie(2) - Claws, Big, +1 to Might skills
 
-
-# Racial Perks
+## Racial Perks ##
 # Darkvision(1) - Can see in low light situations.  No penalties to Perception checks.
 # Natural Armor(1) - +2 Armor value from Scales, cybernetic enhancements or other means integrated into the physical form.
 # Claws(1) - Unarmed attacks gain +2 to damage. +2 to Climb checks
@@ -61,5 +59,18 @@ class Character(DefaultCharacter):
 # Big(1) - Max Might set to 5D, Max Agility set to 3D
 # Small(1) - Max Agility set to 5D, Max Might set to 3D
 
-# General Perks
+## General Perks ##
 # Force Sensitivity(5) - A connection with the Force is possible.  This unlocks the Force Skill.
+
+
+## Force Perks ##
+# Force Jump(1) - Force assisted jump that adds height or length to the jump based on need and Force Skill check. (TN 10) +2D to Acrobatics check
+# Force Speed(1) - Force assisted speed.  Can be used to evade (doubles dodge roll for 1 round) (TN 10) +2D to Running check
+# Telepathy(1) - Ability to send(TN 15) and receive(TN 10) telepathic messages. Resist=Wit
+# Telekinesis(1) - Ability to lift 10kg x Force die with only the Force. (TN 15)  To use as a thrown weapon, check Throwing vs dodge, as per normal rules)
+# Force Parry(2) - Ability to use the force to guide your hand when attempting to Parry an incoming attack.  Force Skill is used in lieu of Melee for Parrying
+# Force Grip(3) - Causes target to drop held weapon at their own feet.  Can be picked up on next turn if not otherwise prevented.  Resist=Agility TN=15
+# Force Slam(3) - The Force manipulates air molecules to forms a gust of wind that slams into a target. Resist=Might TN=15 +3D to throwing 2D damage
+# Force Choke(4) - The Force compresses the air molecules around the targets neck to compress their ability to breath.  Resist=Might TN=20 +2D to brawling 3D damage
+# Force Lightning(5) - The Force charges particles in the air forming lightning from the user to the target.  TN=25 +3D to Firearms Resist=Agility 4D Damage
+# Mind Trick(3) - The Force enhances the user's ability to influence others. TN=15 +1D to Charm skills Resist=Wit
