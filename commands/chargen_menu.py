bb24d463268aeccf3d1411b7a1849baf07893730
grungies1138 <grungies1138@gmail.com>
2019-04-12 09:34:26 -0500

    Chargen

--- commands/chargen_menu.py
+++ commands/chargen_menu.py
@@ -265,6 +265,7 @@ def confirm_species(caller, caller_input, **kwargs):
             text += "Currently selected perks: {}\n\n".format(", ".join(caller.ndb._menutree.perks))
         else:
             caller.ndb._menutree.perks = [kwargs.get("perk")]
+            text += "Currently selected perks: {}\n\n".format(", ".join(caller.ndb._menutree.perks))
     if kwargs.get("selected_species") == "custom":
         text += "Creating a custom species can be fun and rewarding for players.  Often, the built-in species do not " \
                 "quite satisfy the creative desires of the character customization process.  To create a custom " \
